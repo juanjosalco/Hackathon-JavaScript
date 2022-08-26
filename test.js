d22fa2cb309cf48adf6cc69cd8995f67111ece2e
george lopez <jorge.lopez.mondragon@gmail.com>
2022-08-25 21:46:38 -0500

    test

---
+++ test.js
@@ -0,0 +1,27 @@
+const info = function getInfo() {
+    return fetch('https://rickandmortyapi.com/api/character/2')
+      .then(response => {
+        return response.json();
+      });
+  };
+ let sample = document.getElementById('sample');
+ let img, h2, link;
+ let users =  [];
+ let app = document.getElementById('app');
+ info().then(res => {
+    users = res;
+      i =0;
+      let tarjeta = sample.cloneNode(true);
+      tarjeta.id = 's'+i;
+      app.appendChild(tarjeta);
+
+      img = document.querySelector('#s'+i+ ' img');
+      img.src = users .image;
+
+      h2 = document.querySelector('#s'+i+ ' h2');
+      h2.textContent=users.name;
+
+      tarjeta.style = 'visibility:visible;';
+
+   
+});
